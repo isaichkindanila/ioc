262148805b6399801e80f3465ab4adb081eb0d86
danila <isaichkindanila@gmail.com>
2019-11-08 17:02:08 +0300

    ClassUtils code clean-up
    Signed-off-by: danila <isaichkindanila@gmail.com>

--- src/main/java/com/github/isaichkindanila/ioc/ClassUtils.java
+++ src/main/java/com/github/isaichkindanila/ioc/ClassUtils.java
@@ -1,11 +1,7 @@
 package com.github.isaichkindanila.ioc;
 
-import java.lang.reflect.InvocationTargetException;
-
 class ClassUtils {
     private static final String CLASS_NOT_FOUND = "class %s not found";
-    private static final String CONSTRUCTOR_NOT_FOUND = "constructor not found for %s";
-    private static final String ILLEGAL_ACCESS = "cannot access constructor of %s";
     private static final String CREATION_FAILURE = "failed to create bean %s";
 
     private static RuntimeException fatal(String template, String parameter, Throwable cause) {
@@ -21,24 +17,21 @@ class ClassUtils {
     }
 
     @SuppressWarnings("unchecked")
-    private static Object createBean0(BeanContainer container, BeanInfo info) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+    private static Object createBean0(BeanContainer container, BeanInfo info) throws Exception {
         var beanClass = info.getBeanClass();
         var argClasses = info.getArgClasses();
 
-        var constructor = beanClass.getDeclaredConstructor(argClasses);
+        var constructor = beanClass.getConstructor(argClasses);
         var parameters = container.getParameters(argClasses);
 
+        constructor.setAccessible(true);
         return constructor.newInstance(parameters);
     }
 
     static Object createBean(BeanContainer container, BeanInfo info) {
         try {
             return createBean0(container, info);
-        } catch (NoSuchMethodException e) {
-            throw fatal(CONSTRUCTOR_NOT_FOUND, info.getBeanClass().getName(), e);
-        } catch (IllegalAccessException e) {
-            throw fatal(ILLEGAL_ACCESS, info.getBeanClass().getName(), e);
-        } catch (InstantiationException | InvocationTargetException e) {
+        } catch (Exception e) {
             throw fatal(CREATION_FAILURE, info.getBeanClass().getName(), e);
         }
     }
