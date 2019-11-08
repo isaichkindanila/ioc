b6f6387dea306df32394df6add391e1f3a9e8a45
danila <isaichkindanila@gmail.com>
2019-11-08 12:34:39 +0300

    fixed tests, now they fail
    Signed-off-by: danila <isaichkindanila@gmail.com>

--- src/test/java/com/github/isaichkindanila/ioc/beans/CompositeBean.java
+++ src/test/java/com/github/isaichkindanila/ioc/beans/CompositeBean.java
@@ -1,16 +1,17 @@
 package com.github.isaichkindanila.ioc.beans;
 
 import com.github.isaichkindanila.ioc.annotation.Bean;
+import com.github.isaichkindanila.ioc.interfaces.GreetingInterface;
 
 @Bean
 public class CompositeBean {
-    private final GreetingBean greetingBean;
+    private final GreetingInterface greetingInterface;
 
-    public CompositeBean(GreetingBean greetingBean) {
-        this.greetingBean = greetingBean;
+    public CompositeBean(GreetingInterface greetingInterface) {
+        this.greetingInterface = greetingInterface;
     }
 
     public String getPersonalizedGreeting(String name) {
-        return greetingBean.getGreeting() + ", " + name;
+        return greetingInterface.getGreeting() + ", " + name;
     }
 }
